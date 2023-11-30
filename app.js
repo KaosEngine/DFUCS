f18e814b08eb191f3f7f6d4acbd4d7cec0aaca4c
david grey <Hypnozombie@hotmail.com>
2023-11-30 15:43:04 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,9 @@
+function doTheThang() {
+
+        document.getElementById("main_div").innerHTML = "Ahhhhhhhhhhhhhh! <button onclick=\"doTheThang2()\"> Click Meeee</button> ";
+  }
+
+  function doTheThang2() {
+
+    document.getElementById("main_div").innerHTML = "HAHAHAHAHAHAHAHAH!!!! <button onclick=\"doTheThang()\"> Do it Again!</button> ";
+}
---
+++ charactersheet.js
@@ -0,0 +1,3 @@
+function doTheThang() {
+    document.getElementById("sheet_div").innerHTML = "HAHAHAHAHAHAHAHAH!!!! <button onclick=\"doTheThang()\"> Do it Again!</button> ";
+}
